bc38fdd8765bcc5579d51e5fd2085e8073ab90ee
Altaz Bhanji <abhanji@enquizit.com>
2022-06-02 16:19:45 -0400

    adding code for Portfolio 2 class

--- flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/flask-03-handling-routes/app.py
+++ flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/flask-03-handling-routes/app.py
@@ -1,15 +1,14 @@
 #Import Flask modules
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, render_template, redirect, url_for
 
-#Create an object named app 
+#Create an object named app
 app = Flask(__name__)
 
 # Create a function named home which returns a string 'This is home page for no path, <h1> Welcome Home</h1>' 
 # and assign route of no path ('/')
-@app.route("/")
+@app.route('/')
 def home():
-    return 'This is home page for no path, <h1> Welcome Home</h1>' 
-
+    return 'This is home page for no path, <h1> Welcome Home</h1>'
 
 
 # Create a function named about which returns a formatted string '<h1>This is my about page </h1>' 
@@ -26,13 +25,11 @@ def error():
     return '<h1>Either you encountered an error or you are not authorized.</h1>'
 
 
-
 # Create a function named admin which redirect the request to the error path 
 # and assign to the route of ('/admin')
-@app.route("/admin")
+@app.route('/admin')
 def admin():
     return redirect(url_for('error'))
-
 
 
 # Create a function named greet which return formatted inline html string 
@@ -40,30 +37,25 @@ def admin():
 # @app.route('/<name>')
 # def greet(name):
 #     greet_format=f"""
-# <!DOCTYPE html>
-# <html>
-# <head>
-#     <title>Greeting Page</title>
-# </head>
-# <body>
-#     <h1>Hello, { name }!</h1>
-#     <h1>Welcome to my Greeting Page</h1>
-# </body>
-# </html>
-#     """
+#     <!DOCTYPE html>
+#     <html>
+#     <head>
+#         <title>Greeting Page</title>
+#     </head>
+#     <body>
+#         <h1>Hello, { name }!</h1>
+#         <h1>Welcome to my Greeting Page</h1>
+#     </body>
+#     </html>
+#         """
 #     return greet_format
 
 
-
-
-# Create a function named greet_admin which redirect the request to the hello path with param of 'Master Admin!!!!' 
+# Create a function named greet_admin which redirect the request to the greet path with param of 'Master Admin!!!!' 
 # and assign to the route of ('/greet-admin')
 @app.route('/greet-admin')
 def greet_admin():
     return redirect(url_for('greet', name='Master Admin!!!!'))
-
-
-
 
 # Rewrite a function named greet which uses template file named `greet.html` under `templates` folder 
 # and assign to the dynamic route of ('/<name>'). 
@@ -71,7 +63,6 @@ def greet_admin():
 @app.route('/<name>')
 def greet(name):
     return render_template('greet.html', name=name)
-
 
 
 # Create a function named list10 which creates a list counting from 1 to 10 within `list10.html` 
@@ -82,7 +73,6 @@ def list10():
     return render_template('list10.html')
 
 
-
 # Create a function named evens which show the even numbers from 1 to 10 within `evens.html` 
 # and assign to the route of ('/evens'). 
 # Please find a template html file named `evens.html` which shows a list of even numbers from 1 to 10 under `templates` folder 
@@ -91,7 +81,6 @@ def evens():
     return render_template('evens.html')
 
 
-
 # Add a statement to run the Flask application which can be reached from any host on port 80.
-if __name__== "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
