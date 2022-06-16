75fa6401de95d5e23d008a141e87770b8f376b43
Altaz Bhanji <abhanji@enquizit.com>
2022-06-16 16:27:57 -0400

    added CLI, CFN template

--- Project-001-Roman-Numerals-Converter/install_app.sh
+++ Project-001-Roman-Numerals-Converter/install_app.sh
@@ -7,8 +7,8 @@ cd /home/ec2-user
 FOLDER='https://raw.githubusercontent.com/altazbhanji/my-repository/main/Project-001-Roman-Numerals-Converter'
 wget ${FOLDER}/app.py
 mkdir templates && cd templates
-wget ${FOLDER}/index.html
-wget ${FOLDER}/result.html
+wget ${FOLDER}/templates/index.html
+wget ${FOLDER}/templates/result.html
 cd ..
 python3 app.py
 
