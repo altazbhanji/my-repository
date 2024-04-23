00479e280a9e3737306d449e45938a9096d950ca
Altaz Bhanji <altazbhanji@gmail.com>
2024-04-22 21:09:10 -0400

    add rds flask files

---
+++ 001-roman-numerals-converter/userdata.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+
+# update the OS
+yum update -y
+
+# install python and flask
+yum install python3 -y
+yum install pip -y
+pip3 install flask
+
+# install application files
+cd /home/ec2-user
+mkdir templates
+FOLDER="https://raw.githubusercontent.com/altazbhanji/my-repository/main/001-roman-numerals-converter"
+wget ${FOLDER}/app.py
+wget -P templates ${FOLDER}/templates/index.html
+wget -P templates ${FOLDER}/templates/result.html
+
+# run application
+python3 app.py
+
