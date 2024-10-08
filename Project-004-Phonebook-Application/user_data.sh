d53d3a2fe07eaad9052a3c63394a7c7c3c99cf77
Altaz Bhanji <altazbhanji@gmail.com>
2024-10-07 21:39:25 -0400

    add kittens cfn project

---
+++ Project-004-Phonebook-Application/user_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+yum update -y
+yum install python3 -y
+yum install python-pip -y
+pip3 install Flask==2.3.3
+pip3 install Flask-MySql
+yum install git -y
+echo "phonebook-instance.cbanmzptkrzf.us-east-1.rds.amazonaws.com" > /home/ec2-user/dbserver.endpoint
+cd /home/ec2-user
+git clone https://github.com/altazbhanji/my-repository.git
+python3 /home/ec2-user/my-repository/Project-004-Phonebook-Application/phonebook-app.py
+
