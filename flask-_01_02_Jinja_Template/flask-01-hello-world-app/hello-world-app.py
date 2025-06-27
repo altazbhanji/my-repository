5beb127f9abe0f89a1d21faeba7e6834ede5addb
Altaz Bhanji <altazbhanji@gmail.com>
2025-06-26 21:41:40 -0400

    update jinja files

--- flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,12 +1,15 @@
 from flask import Flask 
+
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Hello World!"
 
-
-
+@app.route("/bye")
+def theend():
+    return "<h1>Good Bye. Comme Again</h1>"
 
 
 if __name__ == '__main__':
-
-    app.run(debug=True, port=30000)
-    # app.run(host= '0.0.0.0', port=8081)
+    app.run(port=8080)
