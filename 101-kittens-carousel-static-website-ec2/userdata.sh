f8743ed270df46d3fc36bc396dc3820b784996e5
Altaz Bhanji <altazbhanji@gmail.com>
2025-08-28 21:25:40 -0400

    add phonebook files

---
+++ 101-kittens-carousel-static-website-ec2/userdata.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+dnf update -y
+dnf install httpd -y
+
+cd /var/www/html/
+
+FOLDER="https://raw.githubusercontent.com/altazbhanji/my-repository/refs/heads/main/101-kittens-carousel-static-website-ec2/static-web"
+
+wget ${FOLDER}/index.html
+wget ${FOLDER}/cat0.jpg
+wget ${FOLDER}/cat1.jpg
+wget ${FOLDER}/cat2.jpg
+
+systemctl start httpd
+systemctl enable httpd
+
